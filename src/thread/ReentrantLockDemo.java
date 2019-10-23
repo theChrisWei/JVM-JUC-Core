748fc9d972a5b33ac610e952a82513052453fa75
Chris Wei <weikang0827@hotmail.com>
2019-10-23 18:20:27 +0800

    initial commit

--- src/thread/ReentrantLockDemo.java
+++ src/thread/ReentrantLockDemo.java
@@ -1,73 +1,93 @@
 package thread;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class ReentrantLockDemo {
-    public static void main(String[] args) {
-        Phone phone=new Phone();
-        syncTest(phone);
-        System.out.println();
 
-        Thread t3=new Thread(phone);
-        Thread t4=new Thread(phone);
-        t3.start();
-        t4.start();
-
-    }
-
-    private static void syncTest(Phone phone) {
-
-        new Thread(()->{
-            try{
-                phone.sendSMS();
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-        },"t1").start();
-
-        new Thread(()->{
-            try{
-                phone.sendSMS();
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-        },"t2").start();
-    }
-}
 class Phone implements Runnable{
-    //Synchronized TEST
-
-    public synchronized void sendSMS(){
-        System.out.println(Thread.currentThread().getId()+"\t"+"sendSMS()");
+    public synchronized void sendSMS() throws Exception {
+        System.out.println(Thread.currentThread().getName() + "\t ---invoked sendSMS()---");
         sendEmail();
     }
-    public synchronized void sendEmail(){
-        System.out.println(Thread.currentThread().getId()+"\t"+"sendEmail()");
+
+    public synchronized void sendEmail() throws Exception {
+        System.out.println(Thread.currentThread().getName() + "\t ---invoked sendEmail()---");
     }
 
-    //Reentrant TEST
 
-    Lock lock=new ReentrantLock();
+    Lock lock = new ReentrantLock();
     @Override
     public void run() {
         get();
     }
-    public void get(){
+
+
+    public void get() {
         lock.lock();
-        try{
-            System.out.println(Thread.currentThread().getId()+"\t"+"get()");
+        try {
+            System.out.println(Thread.currentThread().getName() + "\t ---invoked get()---");
             set();
-        }finally {
+        } finally {
             lock.unlock();
         }
     }
-    public void set(){
+
+    public void set() {
         lock.lock();
-        try{
-            System.out.println(Thread.currentThread().getId()+"\t"+"set()");
-        }finally {
+        try {
+            System.out.println(Thread.currentThread().getName() + "\t ---invoked set()---");
+        } finally {
             lock.unlock();
         }
+    }
+
+}
+
+public class ReentrantLockDemo {
+    public static void main(String[] args) {
+        Phone phone = new Phone();
+        new Thread(() -> {
+            try {
+                phone.sendSMS();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }, "t1").start();
+
+
+        new Thread(() -> {
+            try {
+                phone.sendSMS();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }, "t2").start();
+
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("------------------------------");
+
+        new Thread(phone, "t3").start();
+        new Thread(phone, "t4").start();
+
+/*
+        Output:
+
+        t1	 ---invoked sendSMS()---
+        t1	 ---invoked sendEmail()---
+        t2	 ---invoked sendSMS()---
+        t2	 ---invoked sendEmail()---
+        ------------------------------
+        t3	 ---invoked get()---
+        t3	 ---invoked set()---
+        t4	 ---invoked get()---
+        t4	 ---invoked set()---
+*/
+
     }
 }
